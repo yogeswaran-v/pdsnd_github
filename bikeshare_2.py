893d9cd1f88922cf940e3d5d9e5a6123d49e6e6f
U-ADS\Yogeswaran V <YogeswaranV@A-R90WNVZ8.ads.mckinsey.com>
2020-11-23 13:30:27 +0530

    refactor: reordered function definition

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -75,23 +75,6 @@ def station_stats(df):
     print("\nThis took %s seconds to run." % (time.time() - start_time))
     print('-'*40)
 
-
-def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
-
-    print('\nCalculating Trip Duration...\n')
-    start_time = time.time()
-    
-    # display total travel time
-
-
-    # display mean travel time
-
-
-    print("\nThis took %s seconds to execute." % (time.time() - start_time))
-    print('-'*40)
-
-
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
@@ -109,6 +92,22 @@ def user_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+    
+    # display total travel time
+
+
+    # display mean travel time
+
+
+    print("\nThis took %s seconds to execute." % (time.time() - start_time))
+    print('-'*40)
+
 
 
 def main():
